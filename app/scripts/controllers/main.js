39734313c3e51ab100222755060a98c0357385b0
karlhutt <karlhutt@gmail.com>
2015-09-22 04:43:15 -0300

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name poliApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the poliApp
+ */
+angular.module('poliApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
